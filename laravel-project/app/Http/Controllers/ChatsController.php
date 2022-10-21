0c9de4e0c37576a04cd5c5d3431362ab59e587f6
teradadayo <pingaeli@gmail.com>
2022-10-21 18:42:10 +0900

    first commit

---
+++ laravel-project/app/Http/Controllers/ChatsController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Log;
+use App\Companies;
+use App\ChatsFolder;
+use App\Guests;
+use App\Chats;
+use App\Http\Requests\CompanyFolder;
+use Illuminate\Http\Request;
+
+class ChatsController extends Controller
+{
+
+
+    public function chats_all($guest_id)
+    {
+
+        $chat = Chats::findByChoiceSelectJoinGuestsAll(
+            'guests.name',
+            'chats.guest_id',
+            'chats.message',
+            'chats.created_at',
+        );
+
+        // Log::debug($chat);
+
+
+        return view('chats/index', [
+            'chats' => $chat,
+            'guest_id' => $guest_id
+        ]);
+
+    }
+
+    public function ChatsCreate(Request $req)
+    {
+        // Log::debug($req);
+
+        $id = $req->id;
+        $mes = $req->message;
+
+        $chat = new Chats();
+        $chat->message = $req->message;
+        $chat->guest_id = $id;
+        $result = $chat->save();
+        if(empty($chat)){
+        return redirect()->route('login');
+        }
+        // var_dump($chat);
+        // exit;
+        // Log::debug($chat);
+
+
+
+
+
+        return redirect()->route('chats.index', [
+            'guest_id' => $chat->guest_id
+
+        ]);
+    }
+}
